1392cdde14799bda2adbeeee727783e90b7e0351
EvelynAyu <aininglai123@outlook.com>
2022-10-08 02:09:59 +0100

    项目初始结构创建

---
+++ mall-ware/src/test/java/com/aining/mall/ware/MallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aining.mall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

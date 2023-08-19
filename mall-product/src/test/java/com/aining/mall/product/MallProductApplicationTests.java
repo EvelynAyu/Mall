695be7bc1b7b2a80046193c276350c1092097b45
EvelynAyu <aininglai123@outlook.com>
2023-08-19 15:32:57 +0100

    分布式缓存

--- mall-product/src/test/java/com/aining/mall/product/MallProductApplicationTests.java
+++ mall-product/src/test/java/com/aining/mall/product/MallProductApplicationTests.java
@@ -7,9 +7,12 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.FileInputStream;
@@ -17,6 +20,7 @@ import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
 
 
 @SpringBootTest
@@ -29,6 +33,12 @@ public class MallProductApplicationTests {
 
     @Autowired
     CategoryService categoryService;
+
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
+
+    @Autowired
+    RedissonClient redissonClient;
 
     @Test
     public void contextLoads() {
@@ -47,6 +57,23 @@ public class MallProductApplicationTests {
     public void testFindPath(){
         Long[] catelogPath = categoryService.findCatelogPath(225L);
         log.info("完整路径：{}", Arrays.asList(catelogPath));
+    }
+
+    @Test
+    public void testStringRedisTemplate(){
+        ValueOperations<String, String> opsForValue = stringRedisTemplate.opsForValue();
+        // 保存
+        opsForValue.set("hello", "word_" + UUID.randomUUID().toString());
+        // 查询
+        String hello = opsForValue.get("hello");
+        System.out.println("测试redis:" + hello);
+
+    }
+
+    @Test
+    public void redissonTest(){
+        System.out.println(redissonClient);
+
     }
 
 
